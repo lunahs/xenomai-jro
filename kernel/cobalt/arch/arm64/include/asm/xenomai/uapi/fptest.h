35ecf23860e8492f1af53b1b7dbff7306686d7f2
Gilles Chanteperdrix <gch@xenomai.org>
2015-10-20 15:53:49 -0400

    kernel: arm64: fix fptest macros

--- kernel/cobalt/arch/arm64/include/asm/xenomai/uapi/fptest.h
+++ kernel/cobalt/arch/arm64/include/asm/xenomai/uapi/fptest.h
@@ -48,10 +48,9 @@ static inline void fp_regs_set(int features, unsigned int val)
 							ldp     d28, d29, [%0, #8 * 28]\n\
 							ldp     d30, d31, [%0, #8 * 30]"
 
-				: /* No outputs. */
-				: "r"(&e[0])
-				  : "d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "d11", "d12", "d13", "d14", "d15",
-					"d16", "d17", "d18", "d19", "d20", "d21", "d22", "d23", "d24", "d25", "d26", "d27", "d28", "d29", "d30", "d31", "memory"
+			: /* No outputs. */
+			: "r"(&e[0])
+			: "memory"
 		);
 	}
 
@@ -84,10 +83,9 @@ static inline unsigned int fp_regs_check(int features, unsigned int val,
 							stp     d28, d29, [%0, #8 * 28]\n\
 							stp     d30, d31, [%0, #8 * 30]"
 
-				:  /* No outputs. */
-				: "r"(&e[0])
-				  : "d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "d11", "d12", "d13", "d14", "d15",
-					"d16", "d17", "d18", "d19", "d20", "d21", "d22", "d23", "d24", "d25", "d26", "d27", "d28", "d29", "d30", "d31", "memory"
+			:  /* No outputs. */
+			: "r"(&e[0])
+			: "memory"
 		);
 
 
